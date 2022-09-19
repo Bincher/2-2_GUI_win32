f3243dfac25bb1bcc5dd496ca0c9c82bc0fdfb06
Bincher <98527148+Bincher@users.noreply.github.com>
2022-09-19 21:52:12 +0900

    gui_프로그래밍 3주차-2(사용자 메세지 처리)

--- gui_kit.cpp
+++ gui_kit.cpp
@@ -2,8 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define WM_MYMOVE WM_USER + 1
+
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-LRESULT CALLBACK BtnProc(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK WndProc2(HWND, UINT, WPARAM, LPARAM);
 HINSTANCE _hInstance;
 WNDPROC _fpOldBtnProc;
 
@@ -24,8 +26,13 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszArg, 
 	WndClass.lpszMenuName = NULL;
 	WndClass.lpszClassName = "Hello"; //변경가능 -> 정의 등록
 
+	WndClass2 = WndClass;
+	WndClass.lpfnWndProc = WndProc2;
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH); //화면 색
+	WndClass.lpszClassName = "WND2"; //변경가능 -> 정의 등록
 
 	if (!RegisterClass(&WndClass)) return NULL;
+	if (!RegisterClass(&WndClass2)) return NULL;
 
 	_hInstance = hInstance;
 
@@ -40,9 +47,18 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszArg, 
 		CW_USEDEFAULT,
 		NULL, NULL, hInstance, NULL
 	);
-
+	_hWnd2 = CreateWindow( //실제 윈도우 생성(11개의 인자)
+		"WND2", //이름이 WND2인 클래스를 이용하여 윈도우 생성
+		"World", //윈도우 타이틀
+		WS_OVERLAPPEDWINDOW,
+		320, 0, 320, 240,
+		hWnd, NULL, hInstance, NULL
+	);
+	
 	ShowWindow(hWnd, nCmdShow); //생성된 윈도우를 실제로 출력
 	UpdateWindow(hWnd);
+
+
 
 	//메시지 루프
 	while (GetMessage(&msg, NULL, 0, 0)) //GetMessage()가 false 될 때까지
@@ -55,29 +71,35 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszArg, 
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT mesg, WPARAM wParam, LPARAM lParam)
 {
-	static HWND hBtn;
+	static HWND hWnd2;
 
 	switch (mesg)
 	{
 	case WM_CREATE:
 
-		hBtn = CreateWindow("BUTTON", "click", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
-			0, 0, 100, 100, hWnd, (HMENU)888, _hInstance, NULL);
-
-		_fpOldBtnProc = (WNDPROC)GetWindowLongPtr(hBtn, GWLP_WNDPROC);
-	
-		SetWindowLongPtr(hBtn, GWLP_WNDPROC, (LONG_PTR)BtnProc);
-
+		hWnd2 = CreateWindow("WND2", "자식윈도우",
+			WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+			320, 0, 320, 240, hWnd, NULL, _hInstance, NULL);
 		break;
 
-
 	case WM_MOUSEMOVE:
+		/*
+		int x, y;
 		HDC hdc;
-		char szPos[32];
-		sprintf(szPos, "%03d %03d", LOWORD(lParam), HIWORD(lParam));
-		hdc = GetDC(hWnd);
-		TextOut(hdc, 102, 0, szPos, strlen(szPos));
-		ReleaseDC(hWnd, hdc);
+		x = LOWORD(lParam);
+		y = HIWORD(lParam);
+		hdc = GetDC(hWnd2);
+		TextOut(hdc, x, y, "X", 1);
+		ReleaseDC(hWnd2, hdc);
+		*/
+		/*
+		int x, y;
+		x = LOWORD(lParam);
+		y = HIWORD(lParam);
+		lParam = (x << 16) | y; //스왑
+		*/
+		SendMessage(hWnd2, 2000, wParam, lParam);
+		//PostMessage(HWnd hWnd, UINT Msg, WPARAM wParam, LPARAM lParam)
 		break;
 
 	case WM_DESTROY:
@@ -86,21 +108,24 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT mesg, WPARAM wParam, LPARAM lParam)
 	}
 	return DefWindowProc(hWnd, mesg, wParam, lParam); //기본적인 윈도우 메시지 처리(default)
 }
-LRESULT CALLBACK BtnProc(HWND hWnd, UINT mesg, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK WndProc2(HWND hWnd, UINT mesg, WPARAM wParam, LPARAM lParam)
 {
 	switch (mesg)
 	{
-	case WM_MOUSEMOVE:
+	case 2000:
+		int x, y;
 		HDC hdc;
-		HWND hParent;
-		hParent = GetParent(hWnd);
-		static char szPos[32];
-		sprintf(szPos, "%03d %03d", LOWORD(lParam), HIWORD(lParam));
-		hdc = GetDC(hParent);
-		TextOut(hdc, 102, 32, szPos, strlen(szPos));
-		ReleaseDC(hParent, hdc);
+		x = LOWORD(lParam);
+		y = HIWORD(lParam);
+		hdc = GetDC(hWnd);
+		TextOut(hdc, x, y, "X", 1);
+		ReleaseDC(hWnd, hdc);
 		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return FALSE;
 	}
-	return CallWindowProc(_fpOldBtnProc, hWnd, mesg, wParam, lParam);
+	
+	return DefWindowProc(hWnd, mesg, wParam, lParam);
 }
 //==SetWindowLong()
