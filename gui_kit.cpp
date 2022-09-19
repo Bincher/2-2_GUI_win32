7ff2b3286bfbf8fd186ce6be9377e09e93068a74
Bincher <98527148+Bincher@users.noreply.github.com>
2022-09-19 14:35:55 +0900

    gui_프로그래밍 3주차-1(notification)

--- gui_kit.cpp
+++ gui_kit.cpp
@@ -114,10 +114,10 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT mesg, WPARAM wParam, LPARAM lParam)
 		//BUTTON은 클래스이름, BS_PUSHBUTTON은 버튼 윈도우 스타일 ES_PUSHBUTTON
 		hEdt1 = CreateWindow("EDIT", "HELLO", WS_VISIBLE | WS_CHILD | WS_BORDER
 			| ES_AUTOHSCROLL | ES_AUTOVSCROLL | ES_MULTILINE,
-			100, 0, 200, 100, hWnd, (HMENU)777, _hInstance, NULL);
+			100, 0, 200, 100, hWnd, (HMENU)999, _hInstance, NULL);
 		hEdt2 = CreateWindow("EDIT", "", WS_VISIBLE | WS_CHILD 
 			| ES_AUTOHSCROLL | ES_AUTOVSCROLL | ES_MULTILINE,
-			0, 102, 302, 100, hWnd, (HMENU)666, _hInstance, NULL);
+			0, 102, 302, 100, hWnd, (HMENU)777, _hInstance, NULL);
 		hWnd2 = CreateWindow(
 			"WND2",
 			"World",
@@ -134,23 +134,36 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT mesg, WPARAM wParam, LPARAM lParam)
 		break;
 
 	//1.메뉴항목 선택
-	//2.컨트롤 윈도우에서 부모 윈도우에 알림 코드 보낼 때
+	//2.컨트롤 윈도우에서 부모 윈도우에 알림 코드 보낼 때(wParam) - Notification
+	//BN_CLICKED BN_DBLCLK BN_KILLFOUCS(키보드 포커스를 잃음)
+	//EN_CHANGED EN_SETOCUSE(키보드 포커스를 얻음)
 	//3.단축키 눌렸을 때
 	case WM_COMMAND:
 		int wNofifyCode = HIWORD(wParam);
 		HWND hWndCtrl = (HWND)lParam;
 		int id = LOWORD(wParam);
-		if (id == 888)
+		if (id == 0)
 			MessageBox(hWnd, "안녕하세요", "알림", MB_OK);
 		/*
 		else if (id == 999)
 			MessageBox(hWnd, "홍길동입니다", "알림", MB_OK);
 		*/
-		else if (id == 777)
+		else if (id == 888)
 		{
 			char szMsg[1024];
 			GetWindowText(hEdt1, szMsg, 1024);
 			SetWindowText(hEdt2, szMsg);
+		}
+		else if (id == 999)
+		{
+			int code;
+			code = HIWORD(wParam);
+			if (code == EN_CHANGE)
+			{
+				char szMsg[1024];
+				GetWindowText(hEdt1, szMsg, 1024);
+				SetWindowText(hEdt2, szMsg);
+			}
 		}
 		break;
 	}
