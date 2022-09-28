c31362f24fb558e067607bec883824b837cd170d
Bincher <98527148+Bincher@users.noreply.github.com>
2022-09-28 15:41:56 +0900

    MFC 기초

--- gui_kit.cpp
+++ gui_kit.cpp
@@ -1,32 +1,60 @@
-﻿#include <iostream>
+﻿#include <Windows.h>
 using namespace std;
+/*
+HWND hWnd = CreateWindow(...);
 
-class A
+ShowWindow(Hwnd, int);
+MoveWindow(Hwnd, int, int, int, int);
+WndProc(HWND, UINT, WPARAM, LPARAM);
+SendMessage(HWND, WPARAM, LPARAM);
+
+LRESULT WndProc(HWND, hWnd, UINT msg, WPARAM wParam, LPARAM, lParam)
 {
-public:
-	int d1;
-	int show() { cout << "A" << endl; return 0; };
-};
-class B: public A
-{
-public:
-	int d2;
-	int show() { cout << "B" << endl; return 0; };
-};
-int main(void)
-{
-	A a;
-	B b;
+	static HWND hBtn;
+	switch (msg)
+	{
+	case WM_CREATE:
+		hBtn = CreateWindow("button", "click",
+			WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+			0, 0, 100, 100,
+			hWnd, 
+			GetWindowLong(hWnd, GWL_HINSTANCE), (MENU)999, 0);
+		break;
 
-	cout << sizeof(int) << endl;
-	cout << sizeof(a) << endl;
-	cout << sizeof(b) << endl;
-
-	A* pA;
-	pA = &b;
-	pA->show();
-	
-	system("pause");
-
-	return 0;
+	case WM_MOUSEMOVE:
+		HDC hdc;
+		char szPos;
+		sprintf(szPos, "%03d %03d", LOWORD(lParam), HIWORD(lParam));
+		hdc = GetDC(hWnd);
+		TextOut(hdc, 0, 0, szPos);
+		ReleaseDC(hWnd, hdc);
+		break;
+	}
 }
+*/
+
+/*
+
+class CCmdTarget; // WM_COMMAND 처리
+
+class CWnd : public CCmdTarget
+{
+public:
+	CWnd();
+	~CWnd();
+
+	//윈도우  핸들
+	HWND m_hWnd;
+
+	// 함수
+	void ShowWindow(int);
+	void MoveWindow(int, int, int, int, BOOL);
+	LRESULT WndProc(UINT, WPARAM, LPARAM);
+
+	// 이벤트 핸들러
+	// 또 다른 종류의 함수
+	void onCreate(...); // WM_CREATE
+	void OnMouseMove(...); // WM_MOUSEMOVE
+	void OnLButtonDown(...); // WM_LBUTTONDOWN
+};
+*/
